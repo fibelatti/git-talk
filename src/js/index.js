71be94e3f519572be761e0b4ecbadc9b0028a298
Filipe Belatti <fibelatti@gmail.com>
2018-12-21 10:05:11 +0000

    Updates talk content

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-var gitgraph = new GitGraph({template: defaultTemplate});
+var gitgraph = new GitGraph({ template: defaultTemplate });
 
 gitgraph.template.commit.message.font = "normal 24pt Calibri";
 
@@ -8,15 +8,19 @@ var talk = gitgraph.branch("talk")
   .commit({ message: "Give a quick introduction", detailId: "quick-introduction" });
 
 var agenda = talk.branch("agenda")
+  .commit({ message: "Keep local copy up to date", detailId: "agenda-keeping-up-to-date" })
   .commit({ message: "Show real life examples", detailId: "agenda-real-life-example" })
+  .commit({ message: "Enforce your policies through integrations", detailId: "agenda-integrations-and-policies" })
   .commit({ message: "Show the importance of commit messages", detailId: "agenda-commit-messages" })
-  .commit({ message: "Give an example of the usual work flow", detailId: "agenda-usual-flow" })
-  .commit({ message: "Resolve conflicts", detailId: "agenda-new-challenger" })
-  .commit({ message: "Keep local copy up to date", detailId: "agenda-keeping-up-to-date" });
+  .commit({ message: "Show some handy commands" });
 
 agenda.merge(talk, "").delete();
 
-talk.commit({ message: "Show real life examples", detailId: "real-life-examples" }).commit();
+talk.commit({ message: "Keep local copy up to date", detailId: "keeping-sync" });
+
+talk.commit({ message: "Show real life examples", detailId: "real-life-examples" });
+
+talk.commit({ message: "Enforce your policies through integrations", detailId: "integrations-and-policies" }).commit();
 
 var commitMessages = talk.branch("commit-messages")
   .commit({ message: "Show the importance of commit messages", detailId: "commit-messages" })
@@ -24,14 +28,16 @@ var commitMessages = talk.branch("commit-messages")
 
 commitMessages.merge(talk, "").delete();
 
-talk.commit({ message: "Give an example of the usual work flow", detailId: "usual-flow" });
+var commands = talk.branch("useful-commands")
+  .commit({ message: "Keep it clean and tidy", detailId: "command-cleanup" })
+  .commit({ message: "Looking for code", detailId: "command-log-s" })
+  .commit({ message: "Come out, come out wherever you are", detailId: "command-bisect" })
+  .commit({ message: "Update a .gitignore file", detailId: "command-ls-files" });
 
-talk.commit({ message: "Resolve conflicts", detailId: "conflict-resolution" });
+commands.merge(talk, "").delete();
 
-talk.commit({ message: "Keep local copy up to date", detailId: "keeping-sync" });
+talk.commit({ message: "Add sources and recommended reading", detailId: "sources-and-recommended-reads" });
 
-talk.commit({ message: "Recommend further reading", detailId: "sources-and-recommended-reads" });
+talk.commit("Answer questions");
 
-talk.commit("Questions?");
-
-talk.commit("Finishes talk");
+talk.commit().commit("Finish talk");
